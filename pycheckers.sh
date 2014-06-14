0eb6a9cef699747b7fd603cb513587d54bc562fe
DOHMATOB Elvis Dopgima <elvis.dohmatob@inria.fr>
2014-06-14 11:44:25 +0200

    ENH: stupid indentation blabla crap

--- pycheckers.sh
+++ pycheckers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+OVER_INDENTED=E126,E128  # silly errors (false +ves) about indentation
+
 epylint.py "$1" 2>/dev/null
 pyflakes "$1"
-pep8 --ignore=E221,E701,E202 --repeat "$1"
+pep8 --ignore=E221,E701,E202,E123,${OVER_INDENTED} --repeat "$1"
 true
